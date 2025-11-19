43641c2bf55e743eacec467e07ba08baeffe04a2
AlmazBag <almazbagmanov@yandex.ru>
2025-11-19 16:25:12 +0300

    Создан начальный экран с кнопкой

---
+++ app/src/main/java/com/bagmanovam/simpleruntest/presentation/home/HomeScreen.kt
@@ -0,0 +1,66 @@
+package com.bagmanovam.simpleruntest.presentation.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    onButtonClick: (Int) -> Unit,
+) {
+
+    var workoutId by remember { mutableStateOf("68") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = workoutId,
+                onValueChange = { workoutId = it },
+                label = { Text("ID тренировки") },
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    isLoading = true
+
+                },
+                enabled = !isLoading
+            ) {
+                if (isLoading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(20.dp),
+                        strokeWidth = 2.dp
+                    )
+                } else {
+                    Text("Загрузить")
+                }
+            }
+        }
+    }
+}
